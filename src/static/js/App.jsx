383e592d04317128c3478732136b8087982c0e50
julien LE THENO <julien.letheno@gmail.com>
2018-02-13 12:10:33 +0100

    Adding nav bar

--- src/static/js/App.jsx
+++ src/static/js/App.jsx
@@ -1,24 +1,35 @@
- import React from "react";
- import Board from "./board/Board"
- import EditingArea from "./editing_area/EditingArea"
+import React from "react";
+import Board from "./board/Board";
+import EditingArea from "./editing_area/EditingArea";
 
- export default class App extends React.Component {
-     constructor(props){
-        super(props);
-        this.showOnEditingArea = this.showOnEditingArea.bind(this);
-     }
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.showOnEditingArea = this.showOnEditingArea.bind(this);
+  }
 
-     showOnEditingArea(info){
-         this.editingArea.update(info);
-     }
-     render() {
-         return <div className="App row">
-             <div className="col s4">
-               <Board showOnEditingArea={this.showOnEditingArea} />{" "}
-             </div>
-             <div className="col s8">
-               <EditingArea onRef={ref => (this.editingArea = ref)} />
-             </div>
-           </div>;
-     }
-    }
+  showOnEditingArea(info) {
+    this.editingArea.update(info);
+  }
+  render() {
+    return (
+      <div className="app">
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo">
+              Specification Maker
+            </a>
+          </div>
+        </nav>
+        <div className="MainBoard row">
+          <div className="col s4">
+            <Board showOnEditingArea={this.showOnEditingArea} />{" "}
+          </div>
+          <div className="col s8">
+            <EditingArea onRef={ref => (this.editingArea = ref)} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
