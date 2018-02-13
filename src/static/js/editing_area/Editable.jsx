b917faa1bec714afccf3f87995c89de45160878a
julien LE THENO <julien.letheno@gmail.com>
2018-02-13 14:50:54 +0100

    Adding Editable for Editing Area

---
+++ src/static/js/editing_area/Editable.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+export default class Editable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { editing: false };
+    this.edit = this.edit.bind(this);
+    this.save = this.save.bind(this);
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+  edit() {
+    this.setState({ editing: true });
+  }
+  save() {
+    const value = this.refs.newText.value;
+    this.setState({ editing: false });
+    this.props.rename(value);
+  }
+
+
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
+
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      this.save();
+    }
+  }
+
+  renderNormal() {
+    return <h3 onClick={this.edit}>{this.props.children}</h3>;
+  }
+  renderForm() {
+    return (
+      <div class="input-field col s6" ref={this.setWrapperRef}>
+        <input
+          defaultValue={this.props.children}
+          id="editable"
+          ref="newText"
+          type="text"
+          class="validate"
+        />
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.editing) {
+      return this.renderForm();
+    } else {
+      return this.renderNormal();
+    }
+  }
+}
--- src/static/js/editing_area/EditingArea.jsx
+++ src/static/js/editing_area/EditingArea.jsx
@@ -1,62 +1,77 @@
 import React from "react";
+import Editable from "./Editable";
 
 export default class EditingArea extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      render: false
-    };
-  }
-  componentDidMount() {
-    this.props.onRef(this);
-  }
-  componentWillUnmount() {
-    this.props.onRef(undefined);
-  }
-  update(info) {
-    if (info.command == "show") {
-      this.setState({
-        render: true,
-        type: info.type,
-        index: info.index,
-        object: {
-          name: info.name
-        }
-      });
-    } else {
-      this.setState({ render: false});
-    }
-  }
+                 constructor(props) {
+                   super(props);
+                   this.state = { render: false };
+                   this.renameElement = this.renameElement.bind(this);
+                 }
+                 componentDidMount() {
+                   this.props.onRef(this);
+                 }
+                 componentWillUnmount() {
+                   this.props.onRef(undefined);
+                 }
+                 update(info) {
+                   if (info.command == "show") {
+                     this.setState({
+                       render: true,
+                       type: info.type,
+                       index: info.index,
+                       object: {
+                         name: info.name
+                       }
+                     });
+                   } else {
+                     this.setState({ render: false });
+                   }
+                 }
 
-  renderActor() {
-    return (
-      <div className="EditingArea card-panel">
-        <h2> Editing Area </h2>
-        <h3> Actor : {this.state.object.name} </h3>
-      </div>
-    );
-  }
-  renderUC() {
-    return (
-      <div className="EditingArea card-panel">
-        <h2> Editing Area </h2>
-        <h3> Use Case : {this.state.object.name} </h3>
-      </div>
-    );
-  }
-  render() {
-    if (this.state.render) {
-      if (this.state.type == "actor") {
-        return this.renderActor();
-      } else if (this.state.type == "UC") {
-        return this.renderUC();
-      }
-    } else {
-      return (
-        <div className="EditingArea card-panel">
-          <h2> Editing Area </h2>
-        </div>
-      );
-    }
-  }
-}
+                 renameElement(newText) {
+                   this.setState({
+                     render: this.state.render,
+                     type: this.state.type,
+                     index: this.state.index,
+                     object: {
+                       name: newText
+                     }
+                   });
+                 }
+
+                 renderActor() {
+                   return <div className="EditingArea card-panel col s6">
+                       <h2> Editing Area </h2>
+                       <div className="row">
+                       <h3 className="col s4"> Actor :</h3>
+                       <Editable className="col s8"  key={1} index={1} type="editable" rename={this.renameElement}>
+                         {this.state.object.name}
+                       </Editable>
+                       </div>
+                     </div>;
+                 }
+                 renderUC() {
+                   return <div className="EditingArea card-panel col s8">
+                       <h2> Editing Area </h2>
+                       <h3>
+                         {" "}
+                         Use Case : {
+                           this.state.object.name
+                         }{" "}
+                       </h3>
+                     </div>;
+                 }
+                 render() {
+                   if (this.state.render) {
+                     if (this.state.type == "actor") {
+                       return this.renderActor();
+                     } else if (this.state.type == "UC") {
+                       return this.renderUC();
+                     }
+                   } else {
+                     return <div className="EditingArea card-panel col s8">
+                         <h2> Editing Area </h2>
+                       </div>;
+                   }
+                 }
+               }
