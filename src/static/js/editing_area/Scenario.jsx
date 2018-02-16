d6002709f01d9fe8a31d5f02810b5d56d46fa2c9
julien LE THENO <julien.letheno@gmail.com>
2018-02-16 13:14:32 +0100

    Adding step scenario components

--- src/static/js/editing_area/Scenario.jsx
+++ src/static/js/editing_area/Scenario.jsx
@@ -1,13 +1,58 @@
 import React from "react";
-
+import ScenarioStep from "./ScenarioStep";
 export default class Scenario extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { steps: [] };
+    this.remove = this.remove.bind(this);
+    this.update = this.update.bind(this);
+    this.add = this.add.bind(this);
+  }
+  add() {
+    let newSteps = this.state.steps;
+    newSteps.push({
+      key: this.state.steps.length,
+      actor: null,
+      action: null
+    });
+    //this.props.storeData(this.props.type, newStep);
+    this.setState({ steps: newSteps });
+  }
+
+  remove(i) {
+    let newSteps = this.state.steps;
+    newSteps.splice(i, 1);
+    //this.props.storeData(this.props.type, newStep);
+    this.setState({ paragraphs: newSteps });
+  }
+
+  update(i, field, newText) {
+    let newSteps = this.state.steps;
+    newSteps[i][field] = newText;
+    //this.props.storeData(this.props.type, newPar);
+    this.setState({ steps: newSteps });
   }
 
   render() {
-    return <div id="scenario" className="card-panel red lighten-1">
-    <h3 className="whiteText"> Scenario </h3>
-    </div>;
+    return (
+      <div id="scenario" className="card-panel red lighten-1">
+        <h3 className="whiteText"> Scenario </h3>
+        <div>
+          {this.state.steps.map((object, i) => (
+            <ScenarioStep
+              key={i}
+              index={i}
+              update={this.update}
+              remove={this.remove}
+            >
+              {object}
+            </ScenarioStep>
+          ))}
+        </div>
+        <button onClick={this.add} className="waves-effect waves-light btn">
+          Add new
+        </button>
+      </div>
+    );
   }
 }
---
+++ src/static/js/editing_area/ScenarioStep.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default class ScenarioStep extends React.Component {
+  constructor(props) {
+    super(props);
+    this.save = this.save.bind(this);
+    this.remove = this.remove.bind(this);
+  }
+  save() {
+    this.props.update(this.props.index, "actor", this.refs.actor.value);
+    this.props.update(this.props.index, "action", this.refs.action.value);
+  }
+
+  remove() {
+    if (window.confirm("Are you sure?")) {
+      this.props.remove(this.props.index);
+    }
+  }
+  render() {
+    return (
+      <div className={"ScenarioStep card-panel red lighten-3"}>
+        <div className="input-field col s6">
+          <h4> {this.props.children.key}</h4>
+          <input
+            defaultValue={this.props.children.actor}
+            id="Actor"
+            ref="newText"
+            type="text"
+            onChange={this.save}
+          />
+          <input
+            defaultValue={this.props.children.action}
+            id="Actor"
+            ref="newText"
+            type="text"
+            onChange={this.save}
+          />
+        </div>
+        <button
+          onClick={this.remove}
+          className="waves-effect waves-light btn-flat"
+        >
+          Remove
+        </button>
+      </div>
+    );
+  }
+}
